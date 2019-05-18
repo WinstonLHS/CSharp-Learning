1209705b6b8bb2930d11a3d178722f52bae29637
Singapore 07 <whendidisaythat@gmail.com>
2019-05-18 09:40:48 +0800

    npm installed

---
+++ FirstConsoleApp/myapp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; //console class is in System Library.
+
+namespace myapp //myapp is the name of the package/project
+{
+    class Program
+    {
+        public static string title = "Hello, This is Winston Lim.";
+        private static int whichPart;
+        static void Main(string[] args)
+        {
+            whichPart = 2;
+            var message = string.Format("Welcome to {0} Part {1}-{3}!",title,whichPart);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(title);
+            Console.WriteLine(message);
+        }
+    }
+}
